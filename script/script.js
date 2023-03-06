1e9761314ae41c8d345fbb99d1b45dfa1e1c9825
LaurenMartins <90726744+LaurenMartins@users.noreply.github.com>
2023-03-06 16:24:59 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,87 @@
+var kaykehetero = document.getElementById("button1");
+var kaykegay = document.getElementById("button2");
+var kay = document.getElementById("button3");
+var kaykebiruta = document.getElementById("button4");
+var kaykebobross = document.getElementById("button5");
+var kaykecareca = document.getElementById("button6");
+var kaykecego = document.getElementById("button7");
+var kaykechorando = document.getElementById("button8");
+var kaykeedwardcullen = document.getElementById("button9");
+var kaykefedido = document.getElementById("button10");
+var kaykejimin = document.getElementById("button11");
+var kaykemario = document.getElementById("button12");
+var kaykepipoca = document.getElementById("button13");
+var kaykepoze = document.getElementById("button14");
+
+var texto = document.getElementById("texto")
+
+kaykehetero.onclick = function(){
+    document.getElementById("foto").src="kaykes/kaykenormal.jpg";
+    texto.innerHTML = "Kayke normal"
+}
+
+kaykegay.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke gay.jpg";
+    texto.innerHTML = "Kayke gay"
+}
+
+kay.onclick = function(){
+    document.getElementById("foto").src="kaykes/kay.jpg";
+    texto.innerHTML = "Kay"
+}
+
+kaykebiruta.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke biruta.jpg";
+    texto.innerHTML = "Kayke biruta"
+}
+
+
+kaykebobross.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke bob ross.jpg";
+    texto.innerHTML = "Kayke bob ross"
+}
+
+kaykecareca.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke careca.jpg";
+    texto.innerHTML = "Kayke careca"
+}
+
+kaykecego.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke cego.jpg";
+    texto.innerHTML = "Kayke cego"
+}
+
+kaykechorando.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke chorando.jpg";
+    texto.innerHTML = "Kayke chorando"
+}
+
+kaykeedwardcullen.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke edward cullen.jpg";
+    texto.innerHTML = "Kayke edward cullen"
+}
+
+kaykefedido.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke fedido.jpg";
+    texto.innerHTML = "Kayke fedido"
+}
+
+kaykejimin.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke jimin do bts.jpg";
+    texto.innerHTML = "Kayke jimin do bts"
+}
+
+kaykemario.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke mario games.jpg";
+    texto.innerHTML = "Kayke mario games"
+}
+
+kaykepipoca.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke pipoca.jpg";
+    texto.innerHTML = "Kayke pipoca"
+}
+
+kaykepoze.onclick = function(){
+    document.getElementById("foto").src="kaykes/kayke poze.jpg";
+    texto.innerHTML = "Kayke poze"
+}
