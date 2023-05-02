99134736f10adbf9c3fbdad0040fedf61f5f79cd
LaurenMartins <90726744+LaurenMartins@users.noreply.github.com>
2023-05-02 18:01:36 -0300

    Delete trabalho1 directory

--- trabalho1/atv5/script/script.js
+++
@@ -1,9 +0,0 @@
-function verificarPalindromo() {
-    const palavra = document.getElementById("palavra").value;
-    const palavraInvertida = palavra.split("").reverse().join("");
-    if (palavra === palavraInvertida) {
-      document.getElementById("resultado").innerText = `${palavra} é um palíndromo`;
-    } else {
-      document.getElementById("resultado").innerText = `${palavra} não é um palíndromo.`;
-    }
-  }
